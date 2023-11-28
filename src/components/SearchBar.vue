c4de3960c9d656ea827b194d08b3a3e64dbf98ad
Amr Ashraf <amr.ashraf.1772000@gmail.com>
2023-11-28 22:19:00 +0200

    using stores

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -5,14 +5,14 @@
       <div class="pt-[11px] px-3 pb-3 flex items-center bg-PaleGrey rounded h-[43px] w-[206px] ml-[12px] relative">
         <img src="@/assets/images/location.png" alt="location logo" class="w-5 h-5 mr-[8px]">
         <input type="" 
-        v-model="selectedCity"
-        @focus="toggleDropdown"
+        v-model="searchStore.selectedCity"
+        @focus="searchStore.toggleDropdown"
         placeholder="Where are you going?"
          class="overflow-hidden text-lightGrey bg-PaleGrey text-ellipsis text-[13px] font-normal tracking-[-0.26px] w-[142px] flex flex-col">
         <img src="@/assets/images/arrow-down.png" alt="dropdown" class="w-4 h-4" @click="controlToggle">
-        <div v-if="showDropdown" class="flex flex-col w-[206px] mt-[25px] items-center justify-center text-center text-lightGrey bg-PaleGrey rounded-[10px] shadow-box absolute top-[30px] right-[8px]">
+        <div v-if="searchStore.showDropdown" class="flex flex-col w-[206px] mt-[25px] items-center justify-center text-center text-lightGrey bg-PaleGrey rounded-[10px] shadow-box absolute top-[30px] right-[8px]">
           <ul>
-            <li v-for="city in cities" :key="city.city_name" @click="selectCity(city)" class="text-[13px] font-normal tracking-[0.28px] leading-5 ">
+            <li v-for="city in searchStore.cities" :key="city.city_name" @click="searchStore.selectCity(city)" class="text-[13px] font-normal tracking-[0.28px] leading-5 ">
                {{city.city_name }}
             </li>
           </ul>
@@ -23,8 +23,8 @@
         <img src="@/assets/images/calendar.png" alt="calender" class="w-5 h-5">
         <input
           type="date"
-          v-model="checkInDate" 
-          @change="handleCheckIn"
+          v-model="searchStore.checkInDate" 
+          @change="searchStore.handleCheckIn"
           placeholder="Check in date" 
           class="overflow-hidden text-lightGrey  bg-PaleGrey text-ellipsis text-[13px] font-normal tracking-[-0.26px]">
       </div>
@@ -33,8 +33,8 @@
         <img src="@/assets/images/calendar.png" alt="calender" class="w-5 h-5">
         <input 
         type="date"
-        v-model="checkOutDate"
-        @change="handleCheckOut" 
+        v-model="searchStore.checkOutDate"
+        @change="searchStore.handleCheckOut" 
         placeholder="Check out date" 
         class="overflow-hidden text-lightGrey  bg-PaleGrey text-ellipsis text-[13px] font-normal tracking-[-0.26px]">
         
@@ -44,8 +44,8 @@
         <img src="@/assets/images/user-square.png" alt="guest" class="w-5 h-5">
         <input 
         type="number" 
-        @input="handleNumerofGuests"
-        v-model="numberOfGuests"
+        @input="searchStore.handleNumberOfGuests"
+        v-model="searchStore.numberOfGuests"
         placeholder="Guests" 
         class="overflow-hidden text-lightGrey bg-PaleGrey  text-ellipsis text-[13px] font-normal tracking-[-0.26px]">
       </div>
@@ -54,14 +54,14 @@
         <img src="@/assets/images/single_bed.png" alt="single bed" class="w-6 h-6">
         <input
          type="number"
-         @input="handleNumerofrooms"
-         v-model="numberOfRooms"
+         @input="searchStore.handleNumberOfRooms"
+         v-model="searchStore.numberOfRooms"
          placeholder="Rooms"
          class="overflow-hidden text-lightGrey bg-PaleGrey  text-ellipsis text-[13px] font-normal tracking-[-0.26px]">
       </div>
 
       <button 
-      @click="controlSearch"
+      @click="performSearchAndNavigate"
       class="w-[134px] h-[44px] ml-[15px] flex px-[18px] py-3 justify-center items-center bg-customBlue text-[15px] font-medium leading-5 tracking-[0.3px] rounded-md text-white">
       Search
     </button>
@@ -71,145 +71,33 @@
 </template>
 
 <script>
-  import { ref } from 'vue';
-  import axios from 'axios';
- 
+
+  import { useRouter } from 'vue-router';
+  import { useSearchStore } from '../stores/searchStore';
 
   export default {
 
     
     setup(){
 
-     const showDropdown = ref(false);
-     const selectedCity = ref('');
-     const cities = ref([]);
-     const selectedCityId = ref(null);
-     const checkInDate = ref('');
-     const checkOutDate = ref('');
-     const numberOfGuests = ref();
-     const numberOfRooms = ref();
-     const searchResults = ref([]);
-     
-
+     const router = useRouter();
+     const searchStore = useSearchStore();
      //methods 
-     const LoadCites = async () => {
-      try{
-        const response = await axios.get('https://booking-com15.p.rapidapi.com/api/v1/hotels/searchDestination' ,{
-          params: {query: 'egypt' },
-          headers :{
-            'X-RapidAPI-Key': 'a72f665bcamshb7e14da0f0f9745p1c06fajsn8863b3f69700',
-            'X-RapidAPI-Host': 'booking-com15.p.rapidapi.com'
-          }
-        });
-        const cityData = response.data.data.filter(item => item.search_type === "city");
-        cities.value = cityData;
 
-        
-      }catch(error){
-        console.log("Error fetching the data",error);
+     const performSearchAndNavigate = async () => {
+        await searchStore.controlSearch();
+        router.push('/searchresults')
       }
-     };
-      
-     
 
-     const toggleDropdown = () => {
-      showDropdown.value = !showDropdown.value;
-     }
-
-     const selectCity = (city) => {
-      console.log(city); 
-      selectedCity.value = city.city_name;
-      console.log(selectedCity.value);
-      selectedCityId.value = city.dest_id;
-      console.log(selectedCityId.value);
-      showDropdown.value = false;
-     }
      const controlToggle = () => {
-      toggleDropdown();
-      LoadCites();
+       searchStore.toggleDropdown();
+       searchStore.loadCities();
      }
-     const handleCheckIn = () => {
-
-      const currentDate = new Date();
-      const selectedDate = new Date(checkInDate.value);
-      if (selectedDate < currentDate){
-        checkInDate.value = currentDate.toISOString().split('T')[0];
-      }
-
-     }
-     const handleCheckOut = () => {
-      
-      const checkInDateValue = new Date(checkInDate.value);
-      const checkoutDateValue = new Date(checkOutDate.value);
-      if (checkoutDateValue <= checkInDateValue) {
-       
-        checkOutDate.value = '';
-      }
-
-     }
-
-     const handleNumerofrooms = () => {
-      const rooms = numberOfRooms.value;
-      if(rooms<1){
-        numberOfRooms.value = '';
-      }
-      
-     }
-     const handleNumerofGuests = () => {
-      const guests = numberOfGuests.value
-      if(guests<1){
-        numberOfGuests.value = '';
-      }
-      
-     }
-
-     const controlSearch = async () => {
-      try{
-        const response = await axios.get('https://booking-com15.p.rapidapi.com/api/v1/hotels/searchHotels' ,{
-          params: {
-            dest_id: selectedCityId.value,
-            search_type: 'CITY',
-            arrival_date: checkInDate.value,
-            departure_date: checkOutDate.value,
-            room_qty: numberOfRooms.value, 
-            adults: numberOfGuests.value,
-            
-          },
-          headers :{
-            'X-RapidAPI-Key': 'a72f665bcamshb7e14da0f0f9745p1c06fajsn8863b3f69700',
-            'X-RapidAPI-Host': 'booking-com15.p.rapidapi.com'
-          }
-        });
-        searchResults.value = response.data.data.hotels;
-        console.log(searchResults.value);
-      }catch(error){
-        console.log("Error fetching the hotels",error);
-      }
-     };
      
-
-      return {
-        showDropdown,
-        selectedCity,
-        cities,
-        selectedCityId,
-        checkInDate,
-        checkOutDate,
-        numberOfGuests,
-        numberOfRooms,
-        searchResults,
-        LoadCites,
-        toggleDropdown,
-        selectCity,
+      return { 
+        searchStore,
         controlToggle,
-        handleCheckIn,
-        handleCheckOut,
-        handleNumerofrooms,
-        handleNumerofGuests,
-        controlSearch
-      
-      
-        
+        performSearchAndNavigate
       };
     }
   };
---
+++ src/stores/searchStore.js
@@ -0,0 +1,101 @@
+import { defineStore } from 'pinia';
+import axios from 'axios';
+
+export const useSearchStore = defineStore({
+  id: 'search',
+  state: () => ({
+    showDropdown: false,
+    selectedCity: '',
+    cities: [],
+    selectedCityId: null,
+    checkInDate: '',
+    checkOutDate: '',
+    numberOfGuests: '',
+    numberOfRooms: '',
+    searchResults: [],
+  }),
+  actions: {
+    async loadCities() {
+      try {
+        const response = await axios.get('https://booking-com15.p.rapidapi.com/api/v1/hotels/searchDestination',{
+          params: {query: 'egypt' },
+          headers :{
+            'X-RapidAPI-Key': 'a72f665bcamshb7e14da0f0f9745p1c06fajsn8863b3f69700',
+            'X-RapidAPI-Host': 'booking-com15.p.rapidapi.com'
+          }
+        });
+        const cityData = response.data.data.filter(item => item.search_type === 'city');
+        this.cities = cityData;
+      } catch (error) {
+        console.error('Error fetching the data', error);
+      }
+    },
+
+    toggleDropdown() {
+      this.showDropdown = !this.showDropdown;
+    },
+
+    selectCity(city) {
+      this.selectedCity = city.city_name;
+      console.log(this.selectedCity);
+      this.selectedCityId = city.dest_id;
+      console.log(this.selectedCityId);
+      this.showDropdown = false;
+      console.log(this.showDropdown);
+    },
+
+    handleCheckIn() {
+      const currentDate = new Date();
+      const selectedDate = new Date(this.checkInDate);
+      if (selectedDate < currentDate) {
+        this.checkInDate = currentDate.toISOString().split('T')[0];
+      }
+    },
+  
+    handleCheckOut() {
+      if (this.checkOutDate <= this.checkInDate) {
+        this.checkOutDate = '';
+      }
+    },
+
+    handleNumberOfRooms() {
+      if (this.numberOfRooms < 1) {
+        this.numberOfRooms = '';
+      }
+    },
+
+    handleNumberOfGuests() {
+      if (this.numberOfGuests < 1) {
+        this.numberOfGuests = '';
+      }
+    },
+
+    async controlSearch() {
+      try {
+        const response = await axios.get('https://booking-com15.p.rapidapi.com/api/v1/hotels/searchHotels',{
+          params: {
+            dest_id: this.selectedCityId,
+            search_type: 'CITY',
+            arrival_date: this.checkInDate,
+            departure_date: this.checkOutDate,
+            room_qty: this.numberOfRooms, 
+            adults: this.numberOfGuests,
+          },
+          headers :{
+            'X-RapidAPI-Key': 'a72f665bcamshb7e14da0f0f9745p1c06fajsn8863b3f69700',
+            'X-RapidAPI-Host': 'booking-com15.p.rapidapi.com'
+          }
+        });
+        console.log(this.selectedCityId);
+        console.log(this.checkInDate);
+        console.log(this.checkOutDate);
+        console.log(this.numberOfRooms);
+        console.log(this.numberOfGuests);
+        this.searchResults = response.data.data.hotels;
+        console.log(this.searchResults);
+      } catch (error) {
+        console.error('Error fetching the hotels', error);
+      }
+    },
+  },
+});
