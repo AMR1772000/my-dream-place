fefb2d7c5570e1d632a6e4d07d939f610f110375
Amr Ashraf <amr.ashraf.1772000@gmail.com>
2023-11-29 21:28:22 +0200

    make new component

---
+++ src/components/SearchByProperty.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="w-[295px] h-[119px] rounded-md bg-PaleGrey mt-[104px] pl-[19px] pt-[20px] pr-[17px]">
+    <h1 class="text-newTextBlack text-[16px] font-medium tracking-[0.16px] mb-[16px]">Search by property name</h1>
+    <div class="flex bg-white rounded w-[259px] h-[44px] pt-[11px] pb-[12px] items-center gap-[10px]  border border-solid border-borderColor ">
+      <img src="@/assets/images/search.png" alt="search logo" class="w-5 h-5 ml-[12px]">
+      <input type="text"
+      placeholder="eg. Beach westpalm"
+       class=" h-[21]px text-lightGrey text-[14px] font-normal leading-5 tracking-[0.28px] w-full pr-[12px]">
+    </div> 
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
